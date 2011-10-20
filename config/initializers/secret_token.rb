dfc28fcc0a2f0f3e7687c86b0c175060b442dab9
Miguel Marinhas <miguel.marinhas@gmail.com>
2011-10-20 01:12:55 +0100

    First commit rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myjobs::Application.config.secret_token = '0112d4dc19a4ff663b5223845223f87728ae9f784b8e1fae17015dac69791873a01b64f0f6314e0d19bd5cb392d5e9708327ef0350cf03667517ddf6c586ab63'
